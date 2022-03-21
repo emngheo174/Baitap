95a8a188a8f612a94f86a395f742c1b168967003
emngheo174 <50536185+emngheo174@users.noreply.github.com>
2022-03-21 17:00:04 +0700

    bai 4 5 6

--- bai4.php
+++ bai4.php
@@ -5,11 +5,12 @@ class Product
     public $price;
     public $quantity;
 
-    public function __construct($name, $price, $quantity)
+    public function __construct($name, $price, $quantity,$categoryId)
     {
         $this->name = $name;
         $this->price = $price;
         $this->quantity = $quantity;
+        $this->categoryId = $categoryId;
     }
 }
 
@@ -25,18 +26,18 @@ class Category
     }
 }
 $listProduct = array(
-    new Product('CPU','5000',5),
-    new Product('VGA','5000',10),
-    new Product('Main','15000',20),
-    new Product('Case','15000',40),
+    new Product('CPU','5000',5,1),
+    new Product('VGA','5000',10),1,
+    new Product('Main','15000',20,2),
+    new Product('Case','15000',40,2),
 );
 
 $listCategory = array(
     new Category('1','Computer'),
     new Category('2','Computer1'),
 );
-
-$nameProduct = '15000';
+//bai 4
+$nameProduct = 'CPU';
 function findProduct($nameProduct,$listProduct){
     foreach($listProduct as $key => $val){
         if($val -> name == $nameProduct){
@@ -50,8 +51,9 @@ function findProduct($nameProduct,$listProduct){
 }
 findProduct($nameProduct,$listProduct);
 
-$nameProduct1 = '15000';
-function findProductByPrice($nameProduct,$listProduct){
+//bai 6
+$namePrice = '15000';
+function findProductByPrice($namePrice,$listProduct){
     foreach($listProduct as $key => $val){
         if($val -> price == $nameProduct){
             echo $val->name;
@@ -62,6 +64,20 @@ function findProductByPrice($nameProduct,$listProduct){
     }
     return null;
 }
-findProduct($nameProduct1,$listProduct);
+findProductByPrice($namePrice,$listProduct);
+//bai5
+$nameCategoryId = '1';
+function findProductByCategoriId($nameProduct,$listProduct){
+    foreach($listProduct as $key => $val){
+        if($val -> categoryId == $nameProduct){
+            echo $val->name;
+            echo $val ->price;
+            echo $val ->quantity;
+        }
+        //echo $val->name;
+    }
+    return null;
+}
+findProductByCategoriId($nameCategoryId,$listProduct);
 
 ?>
